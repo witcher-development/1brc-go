523abe37e743ae50149ebbe22b0d10d1c8e15af7
roman <roman@luchs-sec.com>
2024-09-20 01:57:08 +0200

    exotic attempt to make as much stuff concurrent

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"math"
 	"os"
 	"runtime"
 	"runtime/pprof"
-	"slices"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -93,32 +92,53 @@ func main() {
 
 	start := time.Now()
 
-	file, err := os.Open("../1brc/measurements_1b.txt")
+	file, err := os.Open("../1brc/measurements.txt")
 	if err != nil {
 		panic(err)
 	}
 
 	stat, err := file.Stat()
-	routinesCount := runtime.NumCPU() - 1
+	routinesCount := runtime.NumCPU()
 	perRange := stat.Size() / int64(routinesCount)
 	file.Close()
 
 	var cursor int64 = 0
 
-	type Aggregate struct {
-		data map[string]map[float32]int
-		cities []string
+	var mx sync.Mutex
+	// var cities []string
+	type City struct {
+		min float32
+		mean float32
+		max float32
 	}
-	ag := Aggregate{data: make(map[string]map[float32]int)}
+	ag := make(map[string]string)
+	var wg sync.WaitGroup
 
-	type Message struct {
-		data map[string][]float32;
-		index int
-	}
-	c := make(chan Message, 10)
+	dict := make(map[int8][2]int)
+	dict[0] = [2]int{65, 65}
+	dict[1] = [2]int{66, 66}
+	dict[2] = [2]int{67, 67}
+	dict[3] = [2]int{68, 68}
+	dict[4] = [2]int{69, 69}
+	dict[5] = [2]int{70, 70}
+	dict[7] = [2]int{71, 71}
+	dict[8] = [2]int{72, 72}
+	dict[9] = [2]int{73, 73}
+	dict[10] = [2]int{74, 74}
+	dict[11] = [2]int{75, 76}
+	dict[12] = [2]int{77, 78}
+	dict[13] = [2]int{79, 80}
+	dict[14] = [2]int{81, 90}
+	dict[15] = [2]int{91, 2000}
 
 	for cursor < stat.Size() - 1 {
-		go func(cursor int64, perRange int64) {
+		wg.Add(1)
+
+		var index int8 = 0
+		if cursor != 0 {
+			index = int8(cursor / perRange)
+		}
+		go func(index int8) {
 			// fmt.Println("new routine", cursor / perRange, stat.Size() - cursor)
 			file, err := os.Open("../1brc/measurements_1b.txt")
 			if err != nil {
@@ -126,146 +146,101 @@ func main() {
 			}
 			defer func () {
 				file.Close()
-				runtime.GC()
+				wg.Done()
 			}()
 
-			file.Seek(cursor, 0)
 			reader := bufio.NewReader(file)
-
-			if cursor != 0 {
-				_, err := reader.ReadBytes('\n')
-				if err == io.EOF {
-					fmt.Println("EOF")
-					return
-				}
-
-				if err != nil {
-					panic(err)
-				}
-			}
-
 			scanner := bufio.NewScanner(reader)
 
-			lagg := make(map[string][]float32)
+			lag := make(map[string]map[float32]int8)
 
-			var cum int64 = 0
-			// fmt.Println("new routine", cursor / perRange, time.Since(start))
+			lmin := byte(dict[index][0])
+			lmax := byte(dict[index][1])
 			for scanner.Scan() {
-				if cum > cursor + perRange {
-					break
-				}
-				line := scanner.Text()
-				cum += int64(len(line))
+				line := scanner.Bytes()
+				first := line[0]
+				// fmt.Println(first, lmin, lmax, first >= byte(lmin) && byte(lmax) >= first, string(first), string(lmin), string(lmax))
 
-				sep := strings.Index(line, ";")
-				city := line[:sep]
-				temp_raw := line[sep+1:]
+				if first < lmin && lmax < first {
+					continue
+				}
+				text := string(line)
+
+				// break
+
+				sep := strings.Index(text, ";")
+				city := text[:sep]
+				temp_raw := text[sep+1:]
 
 				temp, err := strconv.ParseFloat(temp_raw, 32)
 				if err != nil {
 					panic(err)
 				}
+				temp_32 := float32(temp)
 
-				lagg[city] = append(lagg[city], float32(temp))
+				_, has := lag[city]
+				if (has) {
+					lag[city][temp_32]++
+				} else {
+					lag[city] = make(map[float32]int8)
+					lag[city][temp_32] = 1
+				}
+				// lag[city] = append(lag[city], float32(temp))
+
+				// ag[key] = struct{}{}
 			}
 			// fmt.Println("old routine", cursor / perRange, time.Since(start))
 
-			c <- Message{
-				data: lagg,
-				index: int(cursor / perRange),
+
+			lag2 := make(map[string]string)
+			for city, temps := range lag {
+				var sum float32 = 0
+				var min float32 = 100
+				var max float32 = 0
+				for temp, count := range temps {
+					if (temp < min) {
+						min = temp
+					}
+					if (temp > max) {
+						max = temp
+					}
+					sum += temp * float32(count)
+				}
+				mean := round(round(sum)/float32(len(temps)))
+				lag2[city] = encode(city, min, mean, max, false)
 			}
-			// fmt.Println(time.Since(start))
 
-			// runtime.ReadMemStats(&rtm)
-			// printMemStats(rtm)
-			// runtime.GC()
-			// fmt.Println("--------------------------------")
-			// runtime.ReadMemStats(&rtm)
-			// printMemStats(rtm)
+			fmt.Println(index, time.Since(start))
+			mx.Lock()
+			defer mx.Unlock()
 
-			// fmt.Println("--------------------------------")
-		}(cursor, perRange)
+			//
+			// for city, temps := range lag {
+			// 	var sum float32 = 0
+			// 	for _, temp := range temps {
+			// 		sum += temp
+			// 	}
+			// 	mean := round(round(sum)/float32(len(temps)))
+			// 	ag[city] = encode(city, slices.Min(temps), mean, slices.Max(temps), false)
+			// }
+			for city, str := range lag2 {
+				ag[city] = str
+			}
+		}(index)
 
 		cursor += perRange
 	}
 
-	messagesCounter := 0
-	for message := range c {
-		messagesCounter++
-		// fmt.Println("receive message", message.index, len(message.data))
-		fmt.Println("loop start", message.index, time.Since(start))
-		for city, temps := range message.data {
-			if _, ok := ag.data[city]; !ok {
-				ag.cities = append(ag.cities, city)
-				ag.data[city] = make(map[float32]int)
-			}
-			for _, temp := range temps {
-				if _, ok := ag.data[city][temp]; ok {
-					ag.data[city][temp]++
-				}
-				ag.data[city][temp] = 1
-			}
-			// ag.data[city] = append(ag.data[city], temps...)
-		}
-		fmt.Println("loop end", message.index, time.Since(start))
-		if messagesCounter == routinesCount + 1 {
-			close(c)
-		}
-	}
-	fmt.Println(ag.data)
+	wg.Wait()
 
-	// fmt.Println("after channel")
-
-	// wg.Wait()
-
-	// count, err := lineCounter(file)
-
-	// fmt.Println(time.Since(start))
-	// fmt.Println(count)
-	// return
-	// content_raw, err := os.ReadFile("../1brc/measurements_1b.txt")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// memProfiling(*profFlag, "mem_after_aggregate")
 	//
-	// content := string(content_raw)
-	// fmt.Println("after file read")
+	// slices.Sort(ag.cities)
 	//
-	// var ranges []int64
-	// var cursor int64 = 0
-	// cl := int64(len(content))
+	// memProfiling(*profFlag, "mem_after_sort")
 	//
-	// printMemStats("Start", rtm)
+	// output := "{"
 	//
-	// for true {
-	// 	index := int64(strings.Index(content[cursor:], "\n"))
-	// 	ranges = append(ranges, cursor+index)
-	// 	cursor += index + 1
-	//
-	// 	// if cursor % 10000000 == 0 {
-	// 	// 	memProfiling(*profFlag, "mem_in_loop")
-	// 		// runtime.GC()
-	// 		// fmt.Println("creating ranges", cursor, cl)
-	// 		// printMemStats(string(cursor), rtm)
-	// 	// }
-	// 	if cursor > cl - 1 {
-	// 		break
-	// 	}
-	// }
-	//
-	// memProfiling(*profFlag, "mem_after_file_read")
-	// printMemStats("Start", rtm)
-
-	memProfiling(*profFlag, "mem_after_aggregate")
-
-	slices.Sort(ag.cities)
-
-	memProfiling(*profFlag, "mem_after_sort")
-
-	output := "{"
-
-	fmt.Println("we got here")
-	fmt.Println(time.Since(start))
 	// for i, city := range ag.cities {
 	// 	temps := ag.data[city]
 	// 	var sum float32 = 0
@@ -278,7 +253,7 @@ func main() {
 	// }
 
 
-	output = output + "}"
+	// output = output + "}"
 	//
 	// memProfiling(*profFlag, "mem_after_output")
 	//
